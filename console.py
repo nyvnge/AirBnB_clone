b48ed252807f6b29f1b0544d6b6fe59192391a52
Sanderson Nyange <sandersonnyange@gmail.com>
2023-05-09 04:11:53 -0700

    pycodestyle compliant

---
+++ console.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""addition function"""
+
+def add(x,y):
+    return x+y
+    
+#initialize vars
+num1 = 60
+num2 = 50
+
+sumTwonum = add(num1,num2)
+
+print(num1,num2,sumTwonum)
