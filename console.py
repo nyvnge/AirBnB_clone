c4ee00103bf0d1c250cbfbaffc10933a4e23eea4
Sanderson Nyange <sandersonnyange@gmail.com>
2023-05-14 13:13:23 -0700

    Various changes and updates

---
+++ console.py
@@ -0,0 +1,101 @@
+#!usr/bin/python3
+import cmd
+from models.base_model import BaseModel
+from models import storage
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+
+"""HBNBCommand Class"""
+
+class HBNBCommand(cmd.Cmd):
+
+    prompt = "(hbnb) "
+    __classes = ["BaseModel"
+
+    def do_quit(self, arg):
+        """Quit command to exit the program."""
+        return True
+
+    def do_EOF(self, arg):
+        """EOF signal to exit the program."""
+        return True
+    
+    def emptyLine(self):
+        return
+
+    def do_create(self, arg):
+        args = arg.split()
+
+        if len(args) == 0:
+            print("** class name missing **")
+        elif args[0] not in self.__classes:
+            print("** class doesn't exist **")
+        else:
+            new_object = eval(f"{args[0]}")()
+            print(new_object.id)
+
+    def do_show(self, arg):
+        args = arg.split()
+
+        if len(args) == 0:
+            print("** class name missing **")
+        elif args[0] not in self.__classes:
+            print("** class doesn't exist **")
+        elif len(args) == 1:
+            print("** instance id missing **")
+        elif f"{args[0]}.{args[1}}" not in storage.all():
+            print("** no instance found **")
+        else:
+            prin(storage.all()[f"{args[0]}.{args[1}}"])
+
+    def do_destroy(self, arg):
+        args = arg.split()
+
+        if len(args) == 0:
+            print("** class name missing **")
+        elif args[0] not in self.__classes:
+            print("** class doesn't exist **")
+        elif len(args) == 1:
+            print("** instance id missing **")
+        elif f"{args[0]}.{args[1}}" not in storage.all():
+            print("** no instance found **")
+        else:
+            del storage.all()[f"{args[0]}.{args[1}}"]
+        storage.save
+
+    def do_all(self, arg)
+        args = arg.split()
+
+        if len(args) == 0:
+            print([str(value) for value in storage.all().values()])
+        elif args[0] not in self.__classes:
+            print("** class doesn't exist **")
+        else:
+            print([str(v) for k, v in storage.all().items() if k.startswith([args[0]]))
+
+    def do_update:
+        args = arg.split()
+
+        if len(args) == 0:
+            print("** class name missing **")
+        elif args[0] not in self.__classes:
+            print("** class doesn't exist **")
+        elif len(args) == 1:
+            print("** instance id missing **")
+        elif f"{args[0]}.{args[1}}" not in storage.all():
+            print("** no instance found **")
+        elif len(args) == 2:
+            print("** attribute name missing **")
+        elif len(args) == 3:
+            print("** value missing **")
+
+
+
+
+if __name__ == '__main__':
+    HBNBCommand().endloop()
+
---
+++ models/user.py
@@ -0,0 +1,8 @@
+#!usr/bin/python3
+
+
+class User(BaseModel):
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
