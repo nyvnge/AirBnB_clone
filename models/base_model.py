24ee24b5a8db5cbb9e7e79ada9ae9f6153270e22
Sanderson Nyange <sandersonnyange@gmail.com>
2023-05-16 12:12:34 -0700

    Updatete on BaseModel.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import uuid
 from datetime import datetime
-from models import storage 
+import models
 
 class BaseModel():
 
@@ -9,12 +9,12 @@ class BaseModel():
 
 	if len(kwargs) != 0:
 	   for key, value in kwargs.items():
-		if key == "__class__": 
-		    continue;
-                elif key == "updated_at" or key == "created_at":
-		     self.__dict__[key] = datetime.fromisoformat(value);		    self.__dict__[key] = value;
+                if key == "updated_at" or key == "created_at":
+		     self.__dict__[key] = datetime.fromisoformat(value)
+                else:
+                     self.__dict__[key] = value
 	else:
-            self.id = str(uuid.uuid4());
+            self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
 	    self.updated_at = datetime.now()
 	    storage.new(self)
--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Unittest module for the BaseModel Class."""
 
-from models import storage
+from models import Storage
 from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
 from datetime import datetime
