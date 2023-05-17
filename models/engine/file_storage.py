14cb27678ca601d3d419fdf6053743bb03141d88
Sanderson Nyange <sandersonnyange@gmail.com>
2023-05-16 19:08:49 -0700

    A lot of Changes conducted between 1 am to 5am

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,38 +1,68 @@
 #!/usr/bin/python3
-""" File storage class """
+# -*- coding: utf-8 -*-
+"""File Storage Module
+Module in charge of the storage of the
+classes and their management.
+"""
+
 import json
-from models.base_model import BaseModel
-from models.user import User
-from models.state import State
-from models.city import City
 from models.amenity import Amenity
+from models.base_model import BaseModel
+from models.city import City
 from models.place import Place
 from models.review import Review
+from models.state import State
+from models.user import User
+from os import path
 
 
 class FileStorage:
-    __file_path = "file.json"
+    """File Storage Class
+    File Storage module.
+    Attributes:
+        __file_path (str):Path of the JSON file storing
+            the contents of the `__objects` variable.
+        __objects (dict): Stores all the instances data
+    """
+    __file_path = 'objects.json'
     __objects = {}
 
     def all(self):
+        """Gets the __objects info
+        Returns the content of the `__objects` class attribute.
+        """
         return self.__objects
 
     def new(self, obj):
-        key = f"{obj.__class__.__name__}.{ obj.id}"
+        """Saves a new object in the `__objects` class attribute
+        Args:
+            obj (inst): Object that adds in the `__objects` class attribute
+        Sets in the `__objects` class attribute the instance data
+        with a key as <obj class name>.id.
+        """
+        key = obj.__class__.__name__ + '.' + obj.id
         self.__objects[key] = obj
 
     def save(self):
-        with open(FileStorage.__file_path, "w", encoding="utf-8") as f:
-            d = {k: v.to_dict() for k, v in self.__objects.items()}
-            json.dump(d, f)
+        """Serializes the content of `__objects` class attribute
+        Contents of `__objects` class attribute will be serialized
+        to the path of `__file_path` class attribute in JSON format
+        with the `created_at` and `updated_at` formatted.
+        """
+        json_dict = {}
+        for k, v in self.__objects.items():
+            json_dict[k] = v.to_dict()
+        with open(self.__file_path, mode='w', encoding='utf-8') as f:
+            f.write(json.dumps(json_dict))
 
     def reload(self):
-        try:
-            with open(FileStorage.__file_path, "r", encoding="utf-8") as f:
-                _dict = json.load(f)
-                for obj_dict in _dict.values():
-                    cls_name = obj_dict["__class__"]
-                    del obj_dict["__class__"]
-                    self.new(eval(cls_name)(**obj_dict))
-        except FileNotFoundError:
-            return
+        """Deserializes the JSON file in `__file_path` class attribute
+        If the file on `__file_path` class attribute exists, each object
+        on the file will be deserialized and appended to the `__objects`
+        class attribute like an instance with the object data.
+        """
+        if path.exists(self.__file_path):
+            with open(self.__file_path, mode='r', encoding='utf-8') as f:
+                json_dict = json.loads(f.read())
+                for k, v in json_dict.items():
+                    self.__objects[k] = eval(v['__class__'])(**v)
